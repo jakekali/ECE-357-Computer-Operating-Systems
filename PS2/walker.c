2ab634dc821a1d2d4de86b9a8906249b0076f1c0
john.nycjets@gmail.com <john.nycjets@gmail.com>
2022-10-01 22:34:45 -0400

    recurs works

--- PS2/walker.c
+++ PS2/walker.c
@@ -8,43 +8,39 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <linux/limits.h>
 
-char max_path[1024];
 char walk(char *pathname) {
     struct stat statbuf;
     DIR * dir;
-    strcpy(max_path, pathname);
-    strcat( max_path, "/" );
+    int length;
+    char full_path[PATH_MAX + 1];
+    strcpy(full_path, pathname);
     dir = opendir (pathname);
+  strcat( full_path, "/" );
     struct dirent *openeddir;
-    int i = 0;
-    while(openeddir = readdir(dir)){
-        char * file = openeddir->d_name;
-        if (strcmp(file, ".") == 0 || strcmp(file, "..") == 0)
+ while((openeddir = readdir(dir)) != NULL){
+     char * file = openeddir->d_name;
+    if (strcmp(file, ".") == 0 || strcmp(file, "..") == 0)
             continue;
-        
-        printf("\n file  :%s " , file);
-        char local_path [1024];
-        strcpy(local_path, max_path);
-        strcat(local_path,file);
+    printf("\n file  :%s " , file);
+       char local_path [PATH_MAX + 1];
+       strcpy(local_path, full_path);
+      strcat(local_path, file);
         lstat(local_path, &statbuf);
-
-    //    printf("%d", strerror(errno));
-        
-      //  printf("%x", statbuf.st_ino);
-       if (S_ISDIR(statbuf.st_mode)) {
-            printf("is a directory \n" );
-            walk(local_path);
-        }
-    }
+       printf("%s", local_path);
+     if (S_ISDIR(statbuf.st_mode)) {
+        printf("is a directory \n" );
+          
+         walk(local_path);
+  }
+}
     closedir(dir);
 }
 
 int main(int argc, char *argv[])
 {
-    char string[3]  = "abc";
-    printf("%s", string);
-    char * pathname = "./testtrav/";
+    char * pathname = "./testtrav";
     walk(pathname);
     return 0;
 }
