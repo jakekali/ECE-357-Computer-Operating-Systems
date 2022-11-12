56fe0ed658e15bd8bc7206f2fbe7c018f0bc67e9
John <john.nycjets@gmail.com>
2022-11-11 21:36:51 -0500

    change

--- PS4/catgrepmore.c
+++ PS4/catgrepmore.c
@@ -20,48 +20,75 @@ int i = 2;
 int fd_file = -1;
 jmp_buf int_jb;
 pid_t grepChild, moreChild;
+int *argc_count;
 
 
-
+char *filename;
 // Establishing Pipes
 int fd_m2g[2];
 int fd_g2mr[2];
-
+int grepcount = 1;
 int bytes_wrote = 0;
 
 
 void nextFile(int signum){
-
+if(grepcount < i - 1) {
+    //checking if file was all ready closed and write side of the pipe was closed
     if (fd_file != -1){
-        close(fd_file);
-    }
-    close(fd_m2g[WRITE]);
+        if( close(fd_file) != 0) {
+            fprintf(stderr, "Error closing %s:  %s \n", filename, strerror(errno));
+        }
 
+    }
+     if( close(fd_m2g[WRITE]) != 0) {
+            fprintf(stderr, "Error  (close): %s \n", strerror(errno));
+        }
+}
+     
     if (signum != SIGPIPE) {
-       kill(grepChild, SIGKILL);
-       kill(moreChild, SIGKILL);
+        if(grepcount < i - 1) {
+            //checking if grepChild still exists
+        if(  kill(grepChild, SIGKILL) != 0 ) {
+            fprintf(stderr, "Error (kill): %s \n", strerror(errno));
+        }
+        }
+         if(  kill(moreChild, SIGKILL) != 0 ) {
+            fprintf(stderr, "Error (kill): %s \n", strerror(errno));
+        }
     } 
-    waitpid(grepChild, NULL, 0);
-      waitpid(moreChild, NULL, 0);
+    if(grepcount < i - 1) {
+    if(waitpid(grepChild, NULL, 0) < 0) {
+            fprintf(stderr, "Error with wait  (grep): %s\n", strerror(errno));
+    }
+    }
+    grepcount++;
+   if(waitpid(moreChild, NULL, 0) < 0) {
+    fprintf(stderr, "Error with wait (more): %s\n", strerror(errno));
+   }
     
 
 
     i++;
-
+    if(signum == SIGUSR2) {
+        if(i < *argc_count) {
+            //argc is not accessed here (even though it is global) so we used a reference to argc to check if there 
+            //are no more files left
+        fprintf(stderr, " \n *** SIGUSR2 received, moving on to file %d ***\n", i - 1 );
+        } else {
+             fprintf(stderr, " \n *** SIGUSR2 received, No more files left!  ***\n ");
+        }
+    }
+  
     longjmp(int_jb, 1);
 
 }
 
 void printData(int signum){
-    fprintf(stderr, "We have wrote %d bytes to the pipe, and have proccessed %d files.",  bytes_wrote, i - 1);
+    fprintf(stderr, "\nWe have wrote %d bytes to the pipe, and have proccessed %d files.\n",  bytes_wrote, i - 1);
 }
 
 
 int establishPipe(){
-
-    pipe(fd_m2g);
-    pipe(fd_g2mr);
-    // TODO Check for Errors
 
 
     if((pipe(fd_m2g) == -1) || (pipe(fd_g2mr) == -1)){
@@ -78,19 +105,33 @@ int establishPipe(){
         case 0: // Child 1: GREP
 
             // Closing unused pipes
-            close(fd_m2g[WRITE]);
-            close(fd_g2mr[READ]);
+            if(close(fd_m2g[WRITE]) != 0) {
+                fprintf(stderr, "Error (pipe): %s\n", strerror(errno));
+            }
+            if(close(fd_g2mr[READ]) != 0) {
+              fprintf(stderr, "Error (pipe): %s\n", strerror(errno));
+            }
 
             // Redirecting STDIN and STDOUT
-            dup2(fd_m2g[READ], STDIN_FILENO);
-            dup2(fd_g2mr[WRITE], STDOUT_FILENO);
+            if(dup2(fd_m2g[READ], STDIN_FILENO)  < 0) {
+                fprintf(stderr, "Error with redirection : %s\n", strerror(errno));
+            }
+                    if(dup2(fd_g2mr[WRITE], STDOUT_FILENO)  < 0) {
+                fprintf(stderr, "Error with redirection : %s\n", strerror(errno));
+            }
 
             // Executing GREP
-            execlp("grep", "grep", pattern, NULL);
+          if(execlp("grep", "grep", pattern, NULL) == -1) {
+            fprintf(stderr, "Error with exec : %s\n",  strerror(errno));
+          }
 
             // Closing used pipes
-            close(fd_m2g[READ]);
-            close(fd_g2mr[WRITE]);
+            if(close(fd_m2g[READ]) < 0) {
+                fprintf(stderr, "Error with (pipe) %s", strerror(errno));
+            }
+            if(close(fd_g2mr[WRITE]) < 0) {
+            fprintf(stderr, "Error with (pipe) %s", strerror(errno));
+            }
             // Exit
             exit(0);
 
@@ -106,18 +147,30 @@ int establishPipe(){
         case 0: // Child 2: MORE
 
             // Closing unused pipes
-            close(fd_m2g[READ]);
-            close(fd_m2g[WRITE]);
-            close(fd_g2mr[WRITE]);
+              if(close(fd_m2g[READ])!= 0) {
+                fprintf(stderr, "Error (pipe): %s\n", strerror(errno));
+            }
+            if( close(fd_m2g[WRITE]) != 0) {
+              fprintf(stderr, "Error (pipe): %s\n", strerror(errno));
+            }
+              if( close(fd_g2mr[WRITE]) != 0) {
+              fprintf(stderr, "Error (pipe): %s\n", strerror(errno));
+            }
 
             // Redirecting STDIN
-            dup2(fd_g2mr[READ], STDIN_FILENO);
+            if (dup2(fd_g2mr[READ], STDIN_FILENO) < 0) {
+                    fprintf(stderr, "Error  (pipe redirection): %s", strerror(errno));
+                            }
 
             // Executing MORE
-            execlp("more", "more", NULL);
+            if(execlp("more", "more", NULL) == -1) {
+                 fprintf(stderr, "Error with exec : %s\n",  strerror(errno));
+            };
 
             // Closing used pipes
-            close(fd_g2mr[READ]);
+           if( close(fd_g2mr[READ]) != 0) {
+            fprintf(stderr, "Error :%s", strerror(errno));
+           }
             // Exit
             exit(0);
 
@@ -126,14 +179,21 @@ int establishPipe(){
     }
 
     // Closing unused pipes
-    close(fd_g2mr[READ]);
-    close(fd_g2mr[WRITE]);
-    close(fd_m2g[READ]);
+    if (close(fd_g2mr[READ]) != 0) {
+        fprintf(stderr, "Error :%s", strerror(errno));
+    }
+    if(close(fd_g2mr[WRITE]) != 0) {
+    fprintf(stderr, "Error :%s", strerror(errno));
+    }
+
+    if(close(fd_m2g[READ]) != 0) {
+fprintf(stderr, "Error :%s", strerror(errno));
+    }
 }
 
 
 int main(int argc, char *argv[]) {
-
+        argc_count = &argc;
       pattern = argv[1];
       
 
@@ -162,43 +222,67 @@ sigsetjmp(int_jb, 1);
   
 
     // read from a file and write to fd_m2g[WRITE]
-   
+   if(argc < 3) {
+  
+    fprintf(stderr, "Error : not enough input arguments.\n");
+    exit(EXIT_FAILURE);
+   }
     while(i < argc){
-      
+     
         establishPipe();
 
-        char *filename = argv[i];
+        filename = argv[i];
         fd_file = open(filename, O_RDONLY);
         if(fd_file == -1){
-            perror("Open");
-            return -1;
+
+            //kill currently running processes as the program should be exited 
+            //since it cant be run anymore
+            kill(grepChild, 9);
+            kill(moreChild, 9);
+            fprintf(stderr, "Error : (%s) %s \n", filename, strerror(errno));
+            exit(EXIT_FAILURE);
+            
         }
 
-        while(1){
+        for(;;) {
             char buffer[4096];
             memset(buffer, '\0', 4096);
             int n = read(fd_file, buffer, 4096);
             if(n == 0 || n == -1){
                 break;
             }
-            bytes_wrote += write(fd_m2g[WRITE], buffer, n);        
+
+            int ret_write = write(fd_m2g[WRITE], buffer, n); 
+            if(ret_write != n) {
+                fprintf(stderr, "partial write");
+            }
+            bytes_wrote += ret_write;       
         }
 
      
 
 
-        close(fd_file);
-        close(fd_m2g[WRITE]);
-        wait(NULL);
-        wait(NULL);
+        if(close(fd_file) != 0) {
+    fprintf(stderr, "Error : %s", strerror(errno));
+   }
+    
+         if(close(fd_m2g[WRITE]) != 0) {
+    fprintf(stderr, "Error : %s", strerror(errno));
+   }     
+        waitpid(grepChild, NULL, 0);
+        //if this point is reached grepChild has finished so the files are closed 
+        //and the grepProcess should not exist. This is needed 
+        grepcount++;
+        waitpid(moreChild, NULL, 0);
+
        
         i++;
     }
-   close(fd_file);
-    // Closing used pipe
-    close(fd_m2g[WRITE]);
-    wait(NULL);
-    wait(NULL);
+  
+
+   
+
+    
 
 
 
